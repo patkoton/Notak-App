f0178049bd1f4819aee12200e69d88d78363d5e0
Patience Orukotan <patkoton@yahoo.com>
2022-05-20 16:04:58 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,97 @@
 import React from "react"
-import { useState } from "react";
-import { nanoid } from "nanoid";
-import NoteList from "./components/NoteList";
-import "./index.css";
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+//import { data } from "./data"
+import Split from "react-split"
+import {nanoid} from "nanoid"
 
-
-export default function App()  {
-  const [notes, setNotes] = useState([{
-    id: nanoid(),
-    text: "This is my first note",
-    date: "05/16/2022"
-  },
-  {
-    id: nanoid(),
-    text: "This is my second note",
-    date: "05/19/2022"
-  },
-  {
-    id: nanoid(),
-    text: "This is my third note",
-    date: "05/22/2022"
-  },
-]);
-
-const addNote = (text) => {
-  const date = new Date();
-  const newNote = {
-    id: nanoid(),
-    text: text,
-    date: date.toLocaleDateString()
-  }
-  const newNotes = [...notes, newNote];
-  setNotes(newNotes);
-}
-
-const deleteNote = (id) => {
-  const newNotes = notes.filter((note) => note.id !== id);
-  setNotes(newNotes);
-}
-
-    return (
-      <div className="container">
-        <NoteList notes={notes} 
-        handleAddNote={addNote} 
-        handleDeleteNote={deleteNote}
-        />
-      </div>
+export default function App() {
+    const [notes, setNotes] = React.useState(
+        () => JSON.parse(localStorage.getItem("notes")) || []
     )
-  }
-  
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+        (notes[0] && notes[0].id) || ""
+    )
+    
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
+    
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "Title"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+    
+    function updateNote(text) {
+        // Put the most recently-modified note at the top
+        setNotes(oldNotes => {
+            const newArray = []
+            for(let i = 0; i < oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if(oldNote.id === currentNoteId) {
+                    newArray.unshift({ ...oldNote, body: text })
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
+    }
+    
+    
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
+    }
+    
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+    
+    return (
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[25, 75]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                    deleteNote={deleteNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
+    )
+}
